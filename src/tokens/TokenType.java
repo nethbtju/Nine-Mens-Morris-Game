b4251c2fe2ce0b2dda24e0f87bf83acab815aaa3
karanlut <klut6@student.monash.edu>
2023-04-27 20:58:08 +1000

    TokenType JavaDoc

--- src/tokens/TokenType.java
+++ src/tokens/TokenType.java
@@ -1,5 +1,9 @@
 package tokens;
 
+/**
+ * The types of Token that can be placed by a Player. Used to identify Tokens on the board that
+ * belong to a Player with the same type.
+ */
 public enum TokenType {
   WHITE,
   BLACK
