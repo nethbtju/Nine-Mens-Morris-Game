047fc4f7278c1d279ded9e414c07a9e393cc4767
karanlut <klut6@student.monash.edu>
2023-06-13 20:12:25 +1000

    Fix TwoTokenWinTutorial

--- src/tutorials/TwoTokenWinTutorial.java
+++ src/tutorials/TwoTokenWinTutorial.java
@@ -7,6 +7,7 @@ import actions.SelectTokenAction;
 import gameengine.Game;
 import gameengine.GameBoardGui;
 
+/** A Tutorial for winning the game by reducing the opponent's tokenCount to 2. */
 public class TwoTokenWinTutorial extends TutorialState {
 
   public TwoTokenWinTutorial(
@@ -20,7 +21,7 @@ public class TwoTokenWinTutorial extends TutorialState {
     GameBoardGui currentGameBoard = this.getCurrentGameBoard();
     currentGameBoard.removeAllIntersections();
 
-    int[] indexes = {22, 11, 16};
+    int[] indexes = {0, 2, 22, 11, 16, 17, 18, 7};
     currentGameBoard.addNewIntersections(indexes);
     getCurrentGame().initialiseMillObservers();
   }
@@ -28,15 +29,15 @@ public class TwoTokenWinTutorial extends TutorialState {
   /** Set the required Tokens onto the required Intersections for the given TutorialState. */
   @Override
   public void setTokens() {
-    int[][] coordinates = {{22, 1}, {11, 0}};
+    int[][] coordinates = {{0, 1}, {2, 1}, {22, 1}, {11, 0}, {16, 0}, {18, 0}, {7, 0}};
     super.setTokens(coordinates);
   }
 
   /** Set the Intersections which a Token may move to in a given TutorialState. */
   @Override
   public void setLegalIntersections() {
-    int[] coordinates = {16, 22, 11};
-    int[] highlightCoordinates = {16};
+    int[] coordinates = {0, 2, 22, 18, 17};
+    int[] highlightCoordinates = {18};
     super.setAsTutorialLegal(coordinates);
     super.highLightIntersection(highlightCoordinates);
   }
@@ -44,19 +45,20 @@ public class TwoTokenWinTutorial extends TutorialState {
   /** Enqueue the Actions required for a given Tutorial. */
   @Override
   public void setActionQueue() {
-    Action[] actionList = {new SelectTokenAction(), new MoveTokenAction(), new RemoveTokenAction()};
+    Action[] actionList = {new SelectTokenAction(), new MoveTokenAction()};
     this.getCurrentGame().updateActionQueue(actionList);
   }
 
   /** Set the Player order for a given Tutorial. */
   @Override
   public void setPlayerQueue() {
-    super.updatePlayerQueue(true, 0, 0);
+    super.updatePlayerQueue(true, 4, 3);
   }
 
   @Override
   public void setCaption() {
     GameBoardGui currentGameBoard = this.getCurrentGameBoard();
-    currentGameBoard.changeCaptionString("Form a mill to remove a token to win the game");
+    currentGameBoard.changeCaptionString(
+        "Form a mill to remove all but 2 of the opponent's Tokens and win the game.");
   }
 }
